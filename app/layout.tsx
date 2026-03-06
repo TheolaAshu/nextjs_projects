3b5048a1bf8861db28ed6d9bf3bfccd065460943
codegod <ashutheola@gmail.com>
2026-03-06 20:43:44 +0100

    second commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <header>Happy birthday</header>
         {children}
+        <footer>TO you</footer>
       </body>
     </html>
   );
---
+++ app/posts/page.tsx
@@ -0,0 +1,7 @@
+export default function page() {
+  return (
+    <div className="text-center pt-12">
+      <h1 className="text 3xl capitalize font-bold mb-4"> POSTs</h1>
+    </div>
+  );
+}
