14ec674647fefdc04be6b5346650b673487a8170
codegod <ashutheola@gmail.com>
2026-03-10 21:35:12 +0100

    logo added and footer fixed

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,9 +28,11 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Header />
-        {children}
-        <Footer />
+        <div className="flex flex-col min-h-screen max-w-[900px] mx-auto bg-white ">
+          <Header />
+          {children}
+          <Footer />
+        </div>
       </body>
     </html>
   );
