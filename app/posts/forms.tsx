bee7570af2994f557fede4a7791e214e1253a0fa
codegod <ashutheola@gmail.com>
2026-03-16 00:04:40 +0100

    removed  button

--- app/posts/forms.tsx
+++ app/posts/forms.tsx
@@ -16,12 +16,6 @@ export default function forms() {
         className="border-2 border-gray-400 rounded-lg p-2 m-2"
         type="text"
       />
-      <button
-        className="bg-blue-500 text-white font-bold py-2 px-4 rounded-lg m-3"
-        type="submit"
-      >
-        Submit
-      </button>
     </form>
   );
 }
